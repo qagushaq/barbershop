a11e67ecd99a9ec0a83a009ca891bc4bf377f36d
qagushaq <qwerty24164444@gmail.com>
2019-10-03 21:58:00 +0300

    add db sqlite3 and datetimepicker

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
 
 get '/' do
 	erb "Hello!
@@ -12,6 +12,25 @@ end
 get '/about' do
 	@error = 'Something went wrong!'
 	erb :about
+end
+
+def get_db
+	return SQLite3::Database.new 'barbershop.db'
+end
+
+configure do
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS "Users"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"username" TEXT,
+			"useremail" TEXT,
+			"phonenumber" TEXT,
+			"datetime" TEXT,
+			"barber" TEXT,
+			"color" TEXT
+		)'
+	db.close
 end
 
 get '/visit' do
@@ -31,17 +50,19 @@ post '/visit' do
   :phonenumber => 'phonenumber',
   :datetime => 'the date and time when you come to us :)' }
 
-	@error = "These fields are required: " + hh.select { |key,_| params[key] ==""}.values.join(", ")
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
 	if @error != ''
 	  return erb :visit
 	end
 
+	db = get_db
+	db.execute 'INSERT into Users (username, useremail, phonenumber,datetime, barber, color)
+		VALUES (?, ?, ?, ?, ?,?)', [@username,@useremail,@phonenumber,@datetime,@barber,@color]
 	@title = "Thank you! We are waiting for you!"
 	@message = "Dear #{@username}, we are wating you at #{@datetime}. We are call back to the number: #{@phonenumber} or write to the email: #{@useremail} for details. Color: #{@color} Barber: #{@barber}"
-	f = File.open './public/clients.txt', 'a'
-  	f.write "Client: #{@username}, Phone: #{@phonenumber}, Eemail: #{@useremail} , Date and Time: #{@datetime}, Color: #{@color}. Barber: #{@barber}.\n"
-  f.close
 	erb :message
+	db.close
 end
 
 get '/admin' do
